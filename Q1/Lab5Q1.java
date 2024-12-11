8bb045730e980c721f63870d86e25dcd95692361
it24104354 <it24104354@my.sliit.lk>
2024-12-11 18:47:51 +0530

    Add files via upload

---
+++ Q1/Lab5Q1.java
@@ -0,0 +1,29 @@
+import java util.scanner;
+
+public class IT24104354Lab5Q1 {
+
+        public static void main(string [] args) {
+               
+                  Scanner scanner = new Scanner(System.in);
+
+                  System.out.println("Enter the first integer:");
+                  int num1 = Scanner.nextInt();
+
+                  System.out.println("Enter the second integer:");
+                  int num2 = Scanner.nextInt();
+
+                  System.out.println("Enter the second integer:");
+                  int num3 = Scanner.nextInt();
+
+                  int smallest =Math.min(num1,math.min(num2,num3));
+
+                  int largest  =Math.min(num1,math.min(num2,num3));
+
+                 System.out.println("The smallest number is: " + smallest);
+                 System.out.println("The largest number is: " +  largest);
+
+              Scanner close();
+           }
+       }
+
+
